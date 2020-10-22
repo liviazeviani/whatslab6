184213969e8dbff904389ced9809342d894034df
Lívia <liviapavinizeviani@gmail.com>
2020-10-22 09:31:45 -0300

    Esqueleto WhatsLab

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React from "react";
+import "./styles.css";
+
+
+
+export default function App() {
+  return (
+    <div className="App">
+      <div className="header">
+        <header>
+          <h1>WhatsLab</h1>
+        </header>
+
+        <main>
+          
+
+      
+
+          <section className="box-principal">
+
+          </section>
+        </main>
+
+       
+      </div>
+    </div>
+  );
+}
+
+
