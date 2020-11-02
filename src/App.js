c843876c73e7d66ee60cd76fcab56a00edfeb5c9
liviazeviani <liviapavinizeviani@gmail.com>
2020-11-02 00:48:17 -0300

    atualizacao

--- src/App.js
+++ src/App.js
@@ -1,28 +1,17 @@
 import React from 'react';
-import './App.css';
-import InputMensagem from './Components/inputMensagem/InputMensagem'
-import styled from 'styled-components'
 
-const Container = styled.div`
-    height: 100vh;
-    width: 500px;
-    display: flex;
-    
-    margin: 0 auto;
-    border: 1px solid black;
-    background: linear-gradient(
-    to left,
-    rgba(7,27,82,1) 0%,
-    rgba(0,128,120,1)100%
-  );
-  `
 
-function App() {
-  return (
-    <Container className="App">
-      <InputMensagem />
-    </Container>
-  );
-}
+class App extends React.Component {
+  render (){
+    return (
+      <div>
+        Whatsalab
+  
+      </div>
+    );
+  }
+
+  }
+ 
 
 export default App;
